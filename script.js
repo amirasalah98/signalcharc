0c000c468b1211981d8099df6d43080249aa5a09
amirasalah98 <78090962+amirasalah98@users.noreply.github.com>
2023-08-23 19:18:15 +0300

    add

--- script.js
+++ script.js
@@ -8,5 +8,7 @@ wheelContainer.appendChild(signupText);
 $(document).on('click', '.submit-button', function() {
 
     $(".my-3 .container").css("display", "none");
-
+    $(".main-content2").css("display", "none");
+    $(".signup-text .col-12 ").css("visibility", "hidden");
+    // $(".congrats-banner ").css("margin", "-55px");
 });
